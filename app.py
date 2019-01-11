7503ddd400ac63e1fc6bae40091ee8c9be21a6ac
MasayaKataoka <masaya.kataoka@tier4.jp>
2019-01-11 18:13:44 +0900

    add InstanceManager class

--- app.py
+++ app.py
@@ -5,10 +5,22 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/run', methods=['POST'])
+def face_info():
+    if request.headers['Content-Type'] != 'application/json':
+        return flask.jsonify(res='error'), 400
+    return flask.jsonify(res='ok')
+
+@app.route('/api/delete', methods=['POST'])
+def face_info():
+    if request.headers['Content-Type'] != 'application/json':
+        return flask.jsonify(res='error'), 400
+    return flask.jsonify(res='ok')
+
 
 if __name__ == '__main__':
     app.run()
---
+++ instance_manager.py
@@ -0,0 +1,30 @@
+import json
+from collections import OrderedDict
+import subprocess
+
+class SimulatorInstance:
+    def __init__(self,bin_type,autoware_ip,rosbridge_server_port,proc):
+        self.type = bin_type
+        self.autoware_ip = autoware_ip
+        self.rosbridge_server_port = rosbridge_server_port
+        self.proc = proc
+
+class InstanceManager:
+    def __init__(self,setting_filepath):
+        f = open(setting_filepath, 'r')
+        self.exe_dict = json.load(f)
+        self.instance_list = []
+    def launch(self,bin_type,autoware_ip,rosbridge_server_port):
+        for exe_data in self.exe_dict:
+            if bin_type == exe_data['name']:
+                proc = subprocess.Popen(exe_data['path'])
+                instance = SimulatorInstance(bin_type,autoware_ip,rosbridge_server_port,proc)
+                self.instance_list.append(instance)
+    def terminate(self,autoware_ip):
+        tmp_list = []
+        for instance in self.instance_list:
+            if instance.autoware_ip == autoware_ip:
+                instance.proc.terminate()
+            else:
+                tmp_list.append(instance)
+        self.instance_list = tmp_list
